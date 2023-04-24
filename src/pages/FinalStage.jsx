ba076f0276f6b8d3216d6805d9f41564f81df452
giorgi gengashvili <giorgi.gengashvili0@gmail.com>
2023-04-25 01:16:20 +0400

    add tournirs container in final stage page

--- src/pages/FinalStage.jsx
+++ src/pages/FinalStage.jsx
@@ -31,11 +31,26 @@ export default function FinalStage() {
               <h4>A კატეგორიის საგზურში შედის</h4>
             </div>
             <ul>
-              <li><img src="/assets/final-item-ticket-icon.png" alt="icon"/>ორმხრივი ავიაბილეთი</li>
-              <li><img src="/assets/final-item-hotel-icon.png" alt="icon"/>The Festival in Malta -ს მეინ ივენთის ბაი-ინი</li>
-              <li><img src="/assets/final-item-event-icon.png" alt="icon"/>Holdem ან Omaha ჰაიროლერ ტურნირის ბაი-ინი</li>
-              <li><img src="/assets/final-item-ticket-icon.png" alt="icon"/>სასტუმრო</li>
-              <li><img src="/assets/final-item-ticket-icon.png" alt="icon"/>€500 სახარჯი ფული</li>
+              <li>
+                <img src="/assets/final-item-ticket-icon.png" alt="icon" />
+                ორმხრივი ავიაბილეთი
+              </li>
+              <li>
+                <img src="/assets/final-item-hotel-icon.png" alt="icon" />
+                The Festival in Malta -ს მეინ ივენთის ბაი-ინი
+              </li>
+              <li>
+                <img src="/assets/final-item-event-icon.png" alt="icon" />
+                Holdem ან Omaha ჰაიროლერ ტურნირის ბაი-ინი
+              </li>
+              <li>
+                <img src="/assets/final-item-ticket-icon.png" alt="icon" />
+                სასტუმრო
+              </li>
+              <li>
+                <img src="/assets/final-item-ticket-icon.png" alt="icon" />
+                €500 სახარჯი ფული
+              </li>
             </ul>
           </div>
           <div className="categoryCard">
@@ -44,11 +59,26 @@ export default function FinalStage() {
               <h4>A კატეგორიის საგზურში შედის</h4>
             </div>
             <ul>
-              <li><img src="/assets/final-item-ticket-icon.png" alt="icon"/>ორმხრივი ავიაბილეთი</li>
-              <li><img src="/assets/final-item-hotel-icon.png" alt="icon"/>The Festival in Malta -ს მეინ ივენთის ბაი-ინი</li>
-              <li><img src="/assets/final-item-event-icon.png" alt="icon"/>Holdem ან Omaha ჰაიროლერ ტურნირის ბაი-ინი</li>
-              <li><img src="/assets/final-item-ticket-icon.png" alt="icon"/>სასტუმრო</li>
-              <li><img src="/assets/final-item-ticket-icon.png" alt="icon"/>€500 სახარჯი ფული</li>
+              <li>
+                <img src="/assets/final-item-ticket-icon.png" alt="icon" />
+                ორმხრივი ავიაბილეთი
+              </li>
+              <li>
+                <img src="/assets/final-item-hotel-icon.png" alt="icon" />
+                The Festival in Malta -ს მეინ ივენთის ბაი-ინი
+              </li>
+              <li>
+                <img src="/assets/final-item-event-icon.png" alt="icon" />
+                Holdem ან Omaha ჰაიროლერ ტურნირის ბაი-ინი
+              </li>
+              <li>
+                <img src="/assets/final-item-ticket-icon.png" alt="icon" />
+                სასტუმრო
+              </li>
+              <li>
+                <img src="/assets/final-item-ticket-icon.png" alt="icon" />
+                €500 სახარჯი ფული
+              </li>
             </ul>
           </div>
           <div className="categoryCard">
@@ -57,13 +87,75 @@ export default function FinalStage() {
               <h4>A კატეგორიის საგზურში შედის</h4>
             </div>
             <ul>
-              <li><img src="/assets/final-item-ticket-icon.png" alt="icon"/>ორმხრივი ავიაბილეთი</li>
-              <li><img src="/assets/final-item-hotel-icon.png" alt="icon"/>The Festival in Malta -ს მეინ ივენთის ბაი-ინი</li>
-              <li><img src="/assets/final-item-event-icon.png" alt="icon"/>Holdem ან Omaha ჰაიროლერ ტურნირის ბაი-ინი</li>
-              <li><img src="/assets/final-item-ticket-icon.png" alt="icon"/>სასტუმრო</li>
-              <li><img src="/assets/final-item-ticket-icon.png" alt="icon"/>€500 სახარჯი ფული</li>
+              <li>
+                <img src="/assets/final-item-ticket-icon.png" alt="icon" />
+                ორმხრივი ავიაბილეთი
+              </li>
+              <li>
+                <img src="/assets/final-item-hotel-icon.png" alt="icon" />
+                The Festival in Malta -ს მეინ ივენთის ბაი-ინი
+              </li>
+              <li>
+                <img src="/assets/final-item-event-icon.png" alt="icon" />
+                Holdem ან Omaha ჰაიროლერ ტურნირის ბაი-ინი
+              </li>
+              <li>
+                <img src="/assets/final-item-ticket-icon.png" alt="icon" />
+                სასტუმრო
+              </li>
+              <li>
+                <img src="/assets/final-item-ticket-icon.png" alt="icon" />
+                €500 სახარჯი ფული
+              </li>
             </ul>
           </div>
+        </div>
+      </section>
+
+      <section className="tournirsContainer">
+        <div className="heading">
+          <span className="line"></span>
+          <h4>მოიგე საგზური 30 აპრილის ტურნირებზე</h4>
+        </div>
+        <div className="wrapper">
+          <div className="tournir">
+            <img src="/assets/poker-item-1-ticket-icon.png" />
+            <h5>„Holdem Grinders”</h5>
+            <p>C კატეგორიის 1 საგზური</p>
+          </div>
+          <div className="tournir">
+            <img src="/assets/poker-item-1-ticket-icon.png" />
+            <h5>„Holdem Grinders”</h5>
+            <p>C კატეგორიის 1 საგზური</p>
+          </div>
+          <div className="tournir">
+            <img src="/assets/poker-item-1-ticket-icon.png" />
+            <h5>„Holdem Grinders”</h5>
+            <p>C კატეგორიის 1 საგზური</p>
+          </div>
+
+          <div className="tournir">
+            <img src="/assets/poker-item-2-ticket-icon.png" />
+            <h5>„The Festival in Malta, GTD“</h5>
+            <p>C კატეგორიის 1 საგზური</p>
+            <p>
+              *ტურნირში მონაწილეობის მიღება შეუძლია ნებისმიერ მსურველს 165₾
+              ბაი-ინის გადახდით.
+            </p>
+          </div>
+          <div className="tournir">
+            <img src="/assets/poker-item-2-ticket-icon.png" />
+            <h5>“Cashgame Sharks”</h5>
+            <p>C კატეგორიის 1 საგზური</p>
+            <p>
+              *ტურნირზე მონაწილებას შეძლებენ მხოლოდ ევროპაბეთის ბეჭდის
+              მფლობელები.
+            </p>
+          </div>
+        </div>
+        <div className="paragraphs">
+          <p>* თუ მოთამაშე მოიგებს ბეჭედს ორჯერ ან მეტჯერ, ის The Lord of the Rings ტურნირზემოხვდება ბეჭდების შესაბამისი რაოდენობის სტეკით</p>
+          <p>*ტურნირში მონაწილეობის მიღება შეუძლია ნებისმიერ მსურველს 165₾-იანი ბაი-ინის გადახდით.</p>
         </div>
       </section>
     </Container>
@@ -115,13 +207,13 @@ const Container = styled.div`
     .categoriesWrapper {
       .categoryCard {
         margin: 20px 0;
-        background-color: #25292B;
+        background-color: #25292b;
         border-radius: 14px;
         padding-bottom: 8px;
-       
+
         .heading {
           position: relative;
-          background-color: #2C3234;
+          background-color: #2c3234;
           border-radius: 10px 10px 0 0;
           text-align: center;
           padding: 17px 0;
@@ -141,22 +233,81 @@ const Container = styled.div`
             font-weight: 900;
           }
         }
-        ul{
+        ul {
           padding: 0 12px;
-          li{
-            background-color: #1C1D1E;
+          li {
+            background-color: #1c1d1e;
             border-radius: 10px;
             margin: 3px 0;
             padding: 12px 15px;
             display: flex;
             align-items: center;
-            img{
+            img {
               margin-right: 12px;
               width: 32px;
               height: 32px;
             }
           }
         }
+      }
+    }
+  }
+  .tournirsContainer {
+    background-color: #25292b;
+    border-radius: 14px;
+    margin-top: 25px;
+    padding-bottom: 14px;
+    .heading {
+      position: relative;
+      background-color: #2c3234;
+      border-radius: 10px 10px 0 0;
+      text-align: center;
+      padding: 17px 0;
+      .line {
+        width: 100px;
+        height: 4px;
+        background-color: #ef5a21;
+        border-radius: 0 0 4px 4px;
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        box-shadow: 0px 0px 10px #ef5a21;
+      }
+      h4 {
+        font-size: 14px;
+        font-weight: 900;
+      }
+    }
+    .wrapper {
+      .tournir {
+        position: relative;
+        background-color: #1e2122;
+        border-radius: 12px;
+        padding: 20px;
+        margin: 10px 16px;
+        img {
+          position: absolute;
+          top: 0;
+          right: 15px;
+        }
+        h5 {
+          font-size: 15px;
+          font-weight: 900;
+          margin-bottom: 5px;
+        }
+        p {
+          font-size: 13px;
+        }
+      }
+    }
+    .paragraphs{
+      margin: 0 auto;
+      max-width: 85%;
+      text-align: center;
+      p{
+        font-size: 13px;
+        margin-bottom: 10px;
       }
     }
   }
