838b04bcc7bdde7fd81790c41291c910866b3f21
giorgi gengashvili <giorgi.gengashvili0@gmail.com>
2023-04-24 23:28:16 +0400

    add background image on body

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,4 +1,4 @@
-import {createGlobalStyle} from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
   *{
@@ -7,11 +7,38 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     list-style-type: none;
     text-decoration: none;
-    ${'' /* color: #FFFFFF; */}
+    ${"" /* color: #FFFFFF; */}
   }
   body{
     background-color: #1C1D1E;
+    @media(min-width: 1024px){
+      position: relative;
+      :before {
+          content: "";
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100vh;
+          background-image: url('/assets/desktop-version-bg.png');
+          background-size: cover;
+          background-position: center;
+          opacity: 0.6;
+          z-index: -1;
+      } 
+      #root{
+        background-color: #1C1D1E;
+        max-width: 940px;
+        height: 100vh;
+        overflow-y: scroll;
+        margin: 30px auto;
+        border-radius: 13px;
+        ::-webkit-scrollbar {
+        display: none;
+        }
+      }
+    }
   }
 
-`
+`;
 export default GlobalStyles;
